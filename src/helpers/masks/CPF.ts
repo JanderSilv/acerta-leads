38498ed8fdb2a55629cddf1947722bc647f019c6
Jander Silva <jander.silv@outlook.com>
2022-06-22 19:02:03 -0300

    ✨ feat(helpers/masks): implements CPF mask

---
+++ src/helpers/masks/CPF.ts
@@ -0,0 +1,7 @@
+export const CPFMask = (cpf: string) =>
+  cpf
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1')
