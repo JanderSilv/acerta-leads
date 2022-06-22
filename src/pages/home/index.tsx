86d0730c749b499ec1a714fd80dc135678809008
Jander Silva <jander.silv@outlook.com>
2022-06-22 17:22:40 -0300

    ✨ feat(pages): implements home page visual.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,74 @@
+import { Link } from 'react-router-dom'
+import { MdOutlineModeEditOutline as EditIcon, MdDeleteOutline as DeleteIcon } from 'react-icons/md'
+import { Button, IconButton, Input, InputGroup, LayoutTitle, Title } from 'src/components'
+import { ButtonsCell, FiltersContainer, FiltersWrapper, Table, TableWrapper } from './styles'
+
 const Home = () => {
-  return <main>olá</main>
+  return (
+    <main>
+      <LayoutTitle>Consulta de Leads</LayoutTitle>
+      <FiltersWrapper>
+        <Title as="h2" size={4} fontWeight={400}>
+          Filtros
+        </Title>
+
+        <FiltersContainer>
+          <InputGroup label="Nome">
+            <Input id="name" />
+          </InputGroup>
+          <InputGroup label="CPF">
+            <Input id="cpf" />
+          </InputGroup>
+        </FiltersContainer>
+
+        <Button className="filters__button">Filtrar</Button>
+      </FiltersWrapper>
+
+      <TableWrapper>
+        <Button as={Link} to="/cadastro">
+          Novo Lead
+        </Button>
+        <Table>
+          <thead>
+            <tr>
+              <th />
+              <th>Email</th>
+              <th>Nome</th>
+              <th>CPF</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <ButtonsCell>
+                <IconButton aria-label="Editar" title="Editar">
+                  <EditIcon />
+                </IconButton>
+                <IconButton aria-label="Deletar" title="Deletar">
+                  <DeleteIcon />
+                </IconButton>
+              </ButtonsCell>
+              <td>Jander.silv@outlook.com</td>
+              <td>João da Silva Siqueira</td>
+              <td>055.373.570-54</td>
+            </tr>
+            <tr>
+              <ButtonsCell>
+                <IconButton>
+                  <EditIcon />
+                </IconButton>
+                <IconButton>
+                  <DeleteIcon />
+                </IconButton>
+              </ButtonsCell>
+              <td>Jander.silv@outlook.com</td>
+              <td>João da Silva Siqueira</td>
+              <td>055.373.570-54</td>
+            </tr>
+          </tbody>
+        </Table>
+      </TableWrapper>
+    </main>
+  )
 }
 
 export default Home
---
+++ src/pages/home/styles.ts
@@ -0,0 +1,73 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.div`
+  ${({ theme: { colors, space } }) => css`
+    width: 100%;
+    min-height: 100vh;
+    padding: ${space[7]};
+    background-color: ${colors.blue100};
+  `}
+`
+
+export const FiltersWrapper = styled.section`
+  ${({ theme: { colors, space, radii } }) => css`
+    padding: ${space[3]};
+    border-radius: ${radii.box};
+    background-color: ${colors.white100};
+
+    display: flex;
+    flex-direction: column;
+
+    & > .filters__button {
+      align-self: flex-end;
+    }
+  `}
+`
+export const FiltersContainer = styled.div`
+  ${({ theme: { space } }) => css`
+    margin-block: ${space[4]};
+    display: flex;
+    flex-wrap: wrap;
+    gap: ${space[3]};
+
+    & > * {
+      flex: 1;
+    }
+  `}
+`
+
+export const TableWrapper = styled.section`
+  ${({ theme: { space } }) => css`
+    width: 100%;
+    margin-top: ${space[4]};
+  `}
+`
+export const Table = styled.table`
+  ${({ theme: { space, colors } }) => css`
+    width: 100%;
+    margin-top: ${space[3]};
+    border-collapse: separate;
+    border-spacing: 0 5px;
+
+    & thead tr {
+      color: ${colors.white100};
+      background-color: transparent;
+    }
+    & tbody tr {
+      background-color: ${colors.white100};
+    }
+    & th {
+      font-weight: 500;
+      text-align: left;
+    }
+    & td {
+      color: ${colors.gray100};
+    }
+  `}
+`
+export const ButtonsCell = styled.td`
+  ${({ theme: { space } }) => css`
+    display: flex;
+    gap: ${space[1]};
+  `}
+`
