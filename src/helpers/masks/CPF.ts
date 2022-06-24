33684b4195ce7503475767908166e922e2e98936
Jander Silva <jander.silv@outlook.com>
2022-06-23 22:19:13 -0300

    ✨ feat(helpers/masks): implements CPF unmask

--- src/helpers/masks/CPF.ts
+++ src/helpers/masks/CPF.ts
@@ -1,7 +1,14 @@
-export const CPFMask = (cpf: string) =>
-  cpf
+export const CPFMask = (cpf?: string) => {
+  if (!cpf) return ''
+  return cpf
     .replace(/\D/g, '')
     .replace(/(\d{3})(\d)/, '$1.$2')
     .replace(/(\d{3})(\d)/, '$1.$2')
     .replace(/(\d{3})(\d{1,2})/, '$1-$2')
     .replace(/(-\d{2})\d+?$/, '$1')
+}
+
+export const CPFUnmask = (cpf?: string) => {
+  if (!cpf) return ''
+  return cpf.replaceAll('.', '').replace('-', '')
+}
